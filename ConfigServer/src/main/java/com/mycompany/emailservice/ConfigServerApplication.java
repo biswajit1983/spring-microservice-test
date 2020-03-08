76c479559bfc13d63ef4031f06656f2058fde262
Biswajit Sinha <biswajit@recko.io>
2020-03-08 11:53:18 +0530

    few changes

--- ConfigServer/src/main/java/com/mycompany/emailservice/ConfigServerApplication.java
+++ ConfigServer/src/main/java/com/mycompany/emailservice/ConfigServerApplication.java
@@ -3,9 +3,9 @@ package com.mycompany.emailservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@EnableEurekaClient
+//@EnableEurekaClient
 @EnableConfigServer
 @SpringBootApplication
 public class ConfigServerApplication {
